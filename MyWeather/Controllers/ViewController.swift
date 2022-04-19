aa9575b5280151fa4bf2fda6bb8b371eebfade15
Alexandra Kokareva <mrs.kokareva@gmail.com>
2022-04-20 00:12:51 +0300

    create alert message work

--- MyWeather/Controllers/ViewController.swift
+++ MyWeather/Controllers/ViewController.swift
@@ -1,19 +1,20 @@
-//
-//  ViewController.swift
-//  MyWeather
-//
-//  Created by Sasha on 19.04.22.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var weatherIcon: UIImageView!
+    @IBOutlet var temperature: UILabel!
+    @IBOutlet var feelTemperature: UILabel!
+    @IBOutlet var city: UILabel!
+    @IBOutlet var searchButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func searchPressed() {
+        self.presentSearchAlertController(withTitle: "Enter city name", message: nil, style: .alert)
+    }
+    
 }
 
