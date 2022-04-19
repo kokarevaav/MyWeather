8bf3bbc359c8248fe6006ee16449d99160f4b831
Alexandra Kokareva <mrs.kokareva@gmail.com>
2022-04-19 20:46:20 +0300

    Initial Commit

---
+++ MyWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWeather
+//
+//  Created by Sasha on 19.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
